28cb7a8cac4f34a9ff331b38ac8ca1e9e1288d05
Kim MinJi <145100917+ToBeGoodmaan@users.noreply.github.com>
2024-03-26 22:46:27 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -9,16 +9,16 @@ public class Main {
         int arr[] = new int[N];
         int count = 0;
         
-        for (int i=0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
-        int set = sc.nextInt();
+        int v = sc.nextInt();
         
-        for (int j=0; j<arr.length; j++) {
-            if (set == arr[j]){
+        for (int j = 0; j < arr.length; j++) {
+            if (v == arr[j]) 
                 count++;
-            }
         }
         System.out.println(count);
     }
+    
 }
